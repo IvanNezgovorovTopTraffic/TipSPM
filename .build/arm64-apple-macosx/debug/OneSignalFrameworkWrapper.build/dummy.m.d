2678f24bc9e4173c061835198ae428eb96db6e4f
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2025-12-11 18:26:43 +0400

    delete build

--- .build/arm64-apple-macosx/debug/OneSignalFrameworkWrapper.build/dummy.m.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX26.1.sdk/usr/include/Darwin.modulemap \
-  /Users/ivannezgovorov/Desktop/100APP/библиотеки\ не\ для\ стрипчат\ без\ амплитуды/TipSPM/.build/checkouts/OneSignal-XCFramework/OneSignalFrameworkWrapper/dummy.m
